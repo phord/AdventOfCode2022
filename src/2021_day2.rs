0c690e8eae7a265646d846ec71dbff9180944460
Phil Hord <phord@purestorage.com>
2022-11-30 18:51:51 -0800

    let splitters/parsers work with &[u8] instead of &str
    This makes it more convenient to index the array since str doesn't
    allow array-indexing.  But it's less convenient for some things
    because [u8] doesn't have built-in parsers and won't compare to
    char or strings directly.

---
+++ src/2021_day2.rs
@@ -0,0 +1,81 @@
+use yaah::aoc;
+use crate::*;
+
+#[derive(Debug)]
+enum Movement {
+    Forward(u64),
+    Up(u64),
+    Down(u64),
+    Bad
+}
+
+#[allow(unused)]
+fn split_to_movement(_input: &'static str) -> Vec<Movement> {
+    let parsed = split_to_words(_input);
+    parsed.iter().map(|x| {
+        let dist = parse_u64(x[1]);
+        match x[0] {
+            b"forward" => Movement::Forward(dist),
+            b"up" => Movement::Up(dist),
+            b"down" => Movement::Down(dist),
+            _ => Movement::Bad,
+        }
+    }).collect()
+}
+
+#[aoc(day2, part1)]
+fn day2_part1(_input: &'static str) -> u64 {
+    let moves = split_to_movement(_input);
+    // println!("{:?}", moves);
+    let mut pos: u64 = 0;
+    let mut depth: u64 = 0;
+
+    for m in moves {
+        match m {
+            Movement::Forward(x) => pos += x,
+            Movement::Up(x) => depth -= x,
+            Movement::Down(x) => depth += x,
+            Movement::Bad => println!("FAILED"),
+        }
+    }
+    pos * depth
+}
+
+#[aoc(day2, part2)]
+fn day2_part2(_input: &'static str) -> u64 {
+    let moves = split_to_movement(_input);
+    // println!("{:?}", moves);
+    let mut pos: u64 = 0;
+    let mut aim: u64 = 0;
+    let mut depth: u64 = 0;
+
+    for m in moves {
+        match m {
+            Movement::Forward(x) => {pos += x; depth += aim*x;},
+            Movement::Up(x) => aim -= x,
+            Movement::Down(x) => aim += x,
+            Movement::Bad => println!("FAILED"),
+        }
+    }
+    pos * depth
+}
+
+#[allow(unused)]
+const SAMPLE_DAY2: &str = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+
+#[test]
+fn test_day2_part1() {
+    let ans = day2_part1(SAMPLE_DAY2);
+    assert_eq!(ans, 150);
+}
+
+#[test]
+fn test_day2_part2() {
+    let ans = day2_part2(SAMPLE_DAY2);
+    assert_eq!(ans, 900);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use yaah::{aoc, aoc_lib, aoc_year};
+use yaah::{aoc_lib, aoc_year};
+use std::str;
 
 mod day1;
 mod day2;
@@ -27,19 +28,29 @@ mod day24;
 mod day25;
 
 aoc_year!(2022);
+aoc_lib!(with_benchmarks);
 
-// Split a string containing multiple lines into a vector strings, one per line
+//______________________________________________________
+//                               GENERIC INPUT SPLITTERS
+
+// Split a string containing multiple lines into a vector of &[u8], one per line
 #[allow(unused)]
-pub fn split_to_lines(_input: &'static str) -> Vec<&str> {
-    _input.lines().collect::<Vec<&str>>()
+pub fn split_to_lines(_input: &'static str) -> Vec<&[u8]> {
+    _input
+        .lines()
+        .map(|x| x.as_bytes())
+        .collect::<Vec<&[u8]>>()
 }
 
 // Split a string containing multiple lines into a vector of vectors of words broken on whitespace
 #[allow(unused)]
-pub fn split_to_words(_input: &'static str) -> Vec<Vec<&str>> {
-    _input.lines()
-        .map(|s| s.split_whitespace().collect())
-        .collect::<Vec<Vec<&str>>>()
+pub fn split_to_words(_input: &'static str) -> Vec<Vec<&[u8]>> {
+    _input
+        .lines()
+        .map(|s| s.split_whitespace()
+                        .map(|x| x.as_bytes())
+                        .collect())
+        .collect::<Vec<Vec<&[u8]>>>()
 }
 
 // Split a string containing single integer values into a Vec<u64>
@@ -50,15 +61,23 @@ pub fn split_to_ints(_input: &'static str) -> Vec<u64> {
         .collect::<Vec<u64>>()
 }
 
+//______________________________________________________
+//                                               PARSERS
+#[allow(unused)]
+fn parse_u64(inp: &[u8]) -> u64 {
+    let s = str::from_utf8(inp).expect("invalid utf8 bytes");
+    let x = s.parse::<u64>();
+    assert!(x.is_ok(), "not a number: '{}'", s);
+    x.unwrap()
+}
+
 // Convert a binary string to a u64 value.
 #[allow(unused)]
-fn binary(s: &str) -> u64 {
+fn parse_binary(s: &[u8]) -> u64 {
     let mut res = 0;
-    for c in s.chars() {
+    for c in s {
         res *= 2;
-        if c == '1' { res += 1; }
+        if *c == b'1' { res += 1; }
     }
     res
 }
-
-aoc_lib!(with_benchmarks);
