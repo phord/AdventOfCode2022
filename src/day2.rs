b1e17cff93f49b40a043b89d1a51fd5d3ee112c6
Phil Hord <phord@purestorage.com>
2022-12-01 22:00:12 -0800

    Day1: Cleanup, rustify

--- src/day2.rs
+++ src/day2.rs
@@ -5,62 +5,54 @@ use crate::*;
 
 //------------------------------ PARSE INPUT
 
-fn parse(_input: &'static str) -> Vec<Vec<&[u8]>> {
+fn parse(_input: &'static str) -> Vec<(usize, usize)> {
     split_to_words(_input)
+        .iter()
+        .map(|game| ((game[0][0] - b'A') as usize, (game[1][0] - b'X') as usize) )
+        .collect()
 }
 
 //------------------------------ SOLVE
-//
-//
-/*
-1 A X Rock
-2 B Y Paper
-3 C Z Scissors
-A > Z
-B > X
-C > Y
- */
+
+fn draw(them: usize) -> usize {
+    them
+}
+
+fn win(them: usize) -> usize {
+    (them + 1) % 3
+}
+
+fn lose(them: usize) -> usize {
+    (them + 2) % 3
+}
 
 fn solve(_input: &'static str, part: usize) -> usize {
-    let _inp = parse(_input);
-
-    let mut score = 0;
-    for game in _inp {
-        let them = 1 + (game[0][0] - b'A') as usize;
-        let mut us = 1 + (game[1][0] - b'X') as usize ;
-
-        if part == 2 {
-            if us == 2 { // draw
-                us = them;  // draw
-            } else if us == 1 { // lose
-                us = if them == 1 { 3 } else if them == 2 { 1 } else { 2 };
-            } else {   // win
-                us = if them == 1 { 2 } else if them == 2 { 3 } else { 1 };
+    parse(_input)
+        .iter()
+        .map(|(mut us, them)|
+        {
+            let them = *them;
+            if part == 2 {
+                // For part 2, "us" tells us what to do
+                //   0 = lose, 1 = draw, 2 = win
+                us = match us {
+                    0 => lose(them),
+                    1 => draw(them),
+                    2 => win(them),
+                    _ => todo!(),
+                };
             }
-        }
 
-        score += us;
-        if  them == us {
-            score += 3;
-        } else if
-            (them == 1 && us == 3) ||
-            (them == 2 && us == 1) ||
-            (them == 3 && us == 2) {
-                // score += 0;
-            }
-        else {
-            score += 6;
-        }
-        println!("{} {} {}", them, us, score);
-    }
-
-    score
+            1 + us +
+                if draw(them) == us { 3 }
+                else if win(them) == us { 6 }
+                else { 0 }
+        })
+        .sum()
 }
 
 //------------------------------ PART 1
 
-#[allow(unused)]
-// Uncomment next line when solution is ready
 #[aoc(day2, part1)]
 fn day2_part1(_input: &'static str) -> usize {
     solve(_input, 1)
@@ -73,8 +65,6 @@ fn test_day2_part1() {
 
 //------------------------------ PART 2
 
-#[allow(unused)]
-// Uncomment next line when solution is ready
 #[aoc(day2, part2)]
 fn day2_part2(_input: &'static str) -> usize {
     solve(_input, 2)
