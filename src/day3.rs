bfb0c1234d10bba448a6b93df63d666807b3e581
Phil Hord <phord@purestorage.com>
2022-12-02 21:56:41 -0800

    Day3: Rucksack Reorganization

--- src/day3.rs
+++ src/day3.rs
@@ -2,28 +2,82 @@
 use yaah::aoc;
 #[allow(unused)]
 use crate::*;
+use itertools::Itertools;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 //------------------------------ PARSE INPUT
 
-fn parse(_input: &'static str) -> Vec<u64> {
-    split_to_ints(_input)
+fn parse(_input: &'static str) -> Vec<(Vec<usize>,Vec<usize>)> {
+    let mut sack = Vec::new();
+
+    for line in _input.lines() {
+        let mut row = Vec::new();
+        for ch in line.as_bytes() {
+            row.push((1 + *ch - if *ch >= b'a' {b'a'} else {b'A' - 26}) as usize);
+        }
+        let len = row.len()/2;
+        let row2 = row.clone();
+        let a = row.iter().take(len).map(|x| *x).collect();
+        let b = row2.iter().skip(len).map(|x| *x).collect();
+        sack.push((a,b));
+    }
+
+    sack
 }
 
 //------------------------------ SOLVE
 
-fn solve(_input: &'static str, _part: usize) -> usize {
+fn solve1(_input: &'static str, _part: usize) -> usize {
     let _inp = parse(_input);
-    _part
+    let mut total = 0;
+    for bag in _inp {
+        let (foo, bar) = bag.clone();
+
+        let foo: HashSet<usize> = HashSet::from_iter(foo.iter().cloned());
+        let bar: HashSet<usize> = HashSet::from_iter(bar.iter().cloned());
+
+        let baz: HashSet<&usize> = foo.intersection(&bar).collect();
+        for b in baz {
+            total += *b;
+        }
+    }
+    total
+}
+
+fn solve2(_input: &'static str, _part: usize) -> usize {
+    let _inp = parse(_input);
+    let mut total = 0;
+
+    let groups = _inp.len() / 3;
+
+    for g in 0..groups {
+        let mut set: Vec<HashSet<usize>> = Vec::new();
+
+        for elf in 0..3 {
+            let (foo, bar) = &_inp[g*3 + elf];
+
+            let mut s: HashSet<usize> = HashSet::from_iter(foo.iter().cloned());
+            let bar: HashSet<usize> = HashSet::from_iter(bar.iter().cloned());
+            s.extend(&bar);
+
+            set.push(s);
+        }
+        let ans = set[0].iter().filter(|k| set[1].contains(k)).filter(|k| set[2].contains(k));
+        for b in ans {
+            total += *b;
+        }
+    }
+    total
 }
 
 //------------------------------ PART 1
 
 #[allow(unused)]
-// Uncomment next line when solution is ready
-// #[aoc(day3, part1)]
+#[aoc(day3, part1)]
 fn day3_part1(_input: &'static str) -> usize {
-    let ans = solve(_input, 1);
-    // assert_eq!(ans, _);
+    let ans = solve1(_input, 1);
+    assert_eq!(ans, 8349);
     ans
 }
 
@@ -35,11 +89,10 @@ fn test_day3_part1() {
 //------------------------------ PART 2
 
 #[allow(unused)]
-// Uncomment next line when solution is ready
-// #[aoc(day3, part2)]
+#[aoc(day3, part2)]
 fn day3_part2(_input: &'static str) -> usize {
-    let ans = solve(_input, 2);
-    // assert_eq!(ans, ___);
+    let ans = solve2(_input, 2);
+    assert_eq!(ans, 2681);
     ans
 }
 
@@ -50,7 +103,12 @@ fn test_day3_part2() {
 
 //------------------------------ SAMPLE DATA
 
-const _SAMPLE: &str = "1234";
+const _SAMPLE: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
 
-const _ANS1: usize = 1;
-const _ANS2: usize = 2;
+const _ANS1: usize = 157;
+const _ANS2: usize = 70;
