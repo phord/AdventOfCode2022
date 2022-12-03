4d463acae135b795f6c1ccaa598bcf6b2e1fc721
Phil Hord <phord@purestorage.com>
2022-12-02 22:11:24 -0800

    Cleanup

--- src/day3.rs
+++ src/day3.rs
@@ -2,25 +2,20 @@
 use yaah::aoc;
 #[allow(unused)]
 use crate::*;
-use itertools::Itertools;
 use std::collections::HashSet;
 use std::iter::FromIterator;
 
 //------------------------------ PARSE INPUT
 
-fn parse(_input: &'static str) -> Vec<(Vec<usize>,Vec<usize>)> {
+fn parse(_input: &'static str) -> Vec<Vec<usize>> {
     let mut sack = Vec::new();
 
     for line in _input.lines() {
-        let mut row = Vec::new();
-        for ch in line.as_bytes() {
-            row.push((1 + *ch - if *ch >= b'a' {b'a'} else {b'A' - 26}) as usize);
-        }
-        let len = row.len()/2;
-        let row2 = row.clone();
-        let a = row.iter().take(len).map(|x| *x).collect();
-        let b = row2.iter().skip(len).map(|x| *x).collect();
-        sack.push((a,b));
+        let row: Vec<usize> = line.as_bytes()
+                                .iter()
+                                .map(|ch| (*ch - if *ch >= b'a' {b'a' - 1} else {b'A' - 27}) as usize)
+                                .collect();
+        sack.push(row);
     }
 
     sack
@@ -32,12 +27,12 @@ fn solve1(_input: &'static str, _part: usize) -> usize {
     let _inp = parse(_input);
     let mut total = 0;
     for bag in _inp {
-        let (foo, bar) = bag.clone();
+        let len = bag.len()/2;
+        let row2 = bag.clone();
+        let a: HashSet<usize> = bag.iter().take(len).map(|x| *x).collect();
+        let b: HashSet<usize> = row2.iter().skip(len).map(|x| *x).collect();
 
-        let foo: HashSet<usize> = HashSet::from_iter(foo.iter().cloned());
-        let bar: HashSet<usize> = HashSet::from_iter(bar.iter().cloned());
-
-        let baz: HashSet<&usize> = foo.intersection(&bar).collect();
+        let baz: HashSet<&usize> = a.intersection(&b).collect();
         for b in baz {
             total += *b;
         }
@@ -51,20 +46,18 @@ fn solve2(_input: &'static str, _part: usize) -> usize {
 
     let groups = _inp.len() / 3;
 
+    // For each group of 3 elves
     for g in 0..groups {
         let mut set: Vec<HashSet<usize>> = Vec::new();
 
+        // For each elf
         for elf in 0..3 {
-            let (foo, bar) = &_inp[g*3 + elf];
-
-            let mut s: HashSet<usize> = HashSet::from_iter(foo.iter().cloned());
-            let bar: HashSet<usize> = HashSet::from_iter(bar.iter().cloned());
-            s.extend(&bar);
-
-            set.push(s);
+            let row = &_inp[g*3 + elf];
+            set.push(HashSet::from_iter(row.iter().cloned()));
         }
-        let ans = set[0].iter().filter(|k| set[1].contains(k)).filter(|k| set[2].contains(k));
-        for b in ans {
+        for b in set[0].iter()
+                        .filter(|k| set[1].contains(k))
+                        .filter(|k| set[2].contains(k)) {
             total += *b;
         }
     }
