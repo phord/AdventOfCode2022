cd04ba13a06d3103747370e51d42818200ccb296
Phil Hord <phord@purestorage.com>
2022-12-05 21:12:49 -0800

    Day6: Tuning Trouble

--- src/day6.rs
+++ src/day6.rs
@@ -2,55 +2,53 @@
 use yaah::aoc;
 #[allow(unused)]
 use crate::*;
-
-//------------------------------ PARSE INPUT
-
-fn parse(_input: &'static str) -> Vec<u64> {
-    split_to_ints(_input)
-}
+use std::collections::HashSet;
 
 //------------------------------ SOLVE
 
 fn solve(_input: &'static str, _part: usize) -> usize {
-    let _inp = parse(_input);
-    _part
+    let len = if _part == 1 { 4 } else { 14 };
+    for i in 0.._input.len() {
+        let sub: HashSet<char> = _input[i..i+len].chars().collect();
+        if sub.len() == len {
+            return i+len;
+        }
+    }
+    0
 }
 
 //------------------------------ PART 1
 
-#[allow(unused)]
-// Uncomment next line when solution is ready
-// #[aoc(day6, part1)]
+#[aoc(day6, part1)]
 fn day6_part1(_input: &'static str) -> usize {
     let ans = solve(_input, 1);
-    // assert_eq!(ans, _);
+    assert_eq!(ans, 1766);
     ans
 }
 
 #[test]
 fn test_day6_part1() {
-    assert_eq!(day6_part1(_SAMPLE), _ANS1);
+    assert_eq!(solve("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 1), 7);
+    assert_eq!(solve("bvwbjplbgvbhsrlpgdmjqwftvncz", 1), 5);
+    assert_eq!(solve("nppdvjthqldpwncqszvftbrmjlhg", 1), 6);
+    assert_eq!(solve("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 1), 10);
+    assert_eq!(solve("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 1), 11);
 }
 
 //------------------------------ PART 2
 
-#[allow(unused)]
-// Uncomment next line when solution is ready
-// #[aoc(day6, part2)]
+#[aoc(day6, part2)]
 fn day6_part2(_input: &'static str) -> usize {
     let ans = solve(_input, 2);
-    // assert_eq!(ans, ___);
+    assert_eq!(ans, 2383);
     ans
 }
 
 #[test]
 fn test_day6_part2() {
-    assert_eq!(day6_part2(_SAMPLE), _ANS2);
+    assert_eq!(solve("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 2), 19);
+    assert_eq!(solve("bvwbjplbgvbhsrlpgdmjqwftvncz", 2), 23);
+    assert_eq!(solve("nppdvjthqldpwncqszvftbrmjlhg", 2), 23);
+    assert_eq!(solve("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 2), 29);
+    assert_eq!(solve("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 2), 26);
 }
-
-//------------------------------ SAMPLE DATA
-
-const _SAMPLE: &str = "1234";
-
-const _ANS1: usize = 1;
-const _ANS2: usize = 2;
