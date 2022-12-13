447b2c44621a051a63cdff7343a8b7e301467f51
Phil Hord <phord@purestorage.com>
2022-12-13 00:43:40 -0800

    Day13: Part2

--- src/day13.rs
+++ src/day13.rs
@@ -4,6 +4,7 @@ use yaah::aoc;
 use crate::*;
 
 use std::fmt;
+use std::cmp::Ordering;
 
 //------------------------------ PARSE INPUT
 
@@ -71,58 +72,56 @@ fn parse_tree(s: &str) -> Node {
     assert!(s.len()==0);
     ret
 }
-enum Comp {
-    Less,
-    Equal,
-    Greater
-}
-
-fn cmp_list(left: &Vec<Node>, right: &Vec<Node>) -> Comp {
+fn cmp_list(left: &Vec<Node>, right: &Vec<Node>) -> Ordering {
     for (c, l) in left.iter().enumerate() {
         if c == right.len() {
-            println!("    - Right side ran out of items, so inputs are not in the right order");
-            return Comp::Greater; }
+            // println!("    - Right side ran out of items, so inputs are not in the right order");
+            return Ordering::Greater; }
         else {
             match cmp_ex(l, &right[c]) {
-                Comp::Equal => {},
-                Comp::Less => { println!("    - Left side is smaller, so inputs are in the right order"); return Comp::Less; }
-                Comp::Greater => { println!("    - Right side is smaller, so inputs are NOT in the right order"); return Comp::Greater; }
+                Ordering::Equal => {},
+                Ordering::Less => {
+                    // println!("    - Left side is smaller, so inputs are in the right order");
+                    return Ordering::Less; }
+                Ordering::Greater => {
+                    // println!("    - Right side is smaller, so inputs are NOT in the right order");
+                    return Ordering::Greater; }
                                 // x => return x,
             }
         }
     }
 
     if right.len() > left.len() {
-        println!("- Left side ran out of items, so inputs are in the right order");
-        Comp::Less
+        // println!("- Left side ran out of items, so inputs are in the right order");
+        Ordering::Less
     } else {
-        Comp::Equal
+        Ordering::Equal
     }
 }
 
-fn cmp_ex(left: &Node, right: &Node) -> Comp {
+fn cmp_ex(left: &Node, right: &Node) -> Ordering {
     match (left, right) {
         (Node::List(l), Node::List(r)) => {
-            println!("   - Compare {:?} vs {:?}", &l, &r);
+            // println!("   - Compare {:?} vs {:?}", &l, &r);
             cmp_list(l, r)},
-        (Node::List(l), Node::Number(r)) => {
-            println!("   - Compare {:?} vs {:?}", &l, &r);
-            println!("   - Mixed types; convert right to [{}] and retry comparison", &r);
+        (Node::List(_l), Node::Number(_r)) => {
+            // println!("   - Compare {:?} vs {:?}", &l, &r);
+            // println!("   - Mixed types; convert right to [{}] and retry comparison", &r);
             cmp_ex(&left, &Node::List(vec![right.clone()]))},
-        (Node::Number(l), Node::List(r)) => {
-            println!("   - Compare {:?} vs {:?}", &l, &r);
-            println!("   - Mixed types; convert left to [{}] and retry comparison", &l);
+        (Node::Number(_l), Node::List(_r)) => {
+            // println!("   - Compare {:?} vs {:?}", &l, &r);
+            // println!("   - Mixed types; convert left to [{}] and retry comparison", &l);
             cmp_ex(&Node::List(vec![left.clone()]), &right)},
         (Node::Number(l), Node::Number(r)) => {
-            println!("   - Compare {} vs {}", &l, &r);
-            if l < r { Comp::Less } else if l == r { Comp::Equal } else { Comp::Greater }
+            // println!("   - Compare {} vs {}", &l, &r);
+            if l < r { Ordering::Less } else if l == r { Ordering::Equal } else { Ordering::Greater }
         }
     }
 }
 
 fn cmp(left: &Node, right: &Node) -> bool {
     match cmp_ex(left, right) {
-        Comp::Greater => false,
+        Ordering::Greater => false,
         _ => true,
     }
 }
@@ -152,21 +151,32 @@ fn solve2(input: &'static str) -> usize {
     let pairs = parse(input);
 
     let mut it = pairs.into_iter();
-    let mut count = 0;
-    let mut sum = 0;
+    let mut vec = Vec::new();
     loop {
         let left = it.next();
         if left.is_none() { break; }
         let left = left.unwrap();
         let right = it.next().unwrap();
         it.next();
-        let l = parse_tree(left);
-        let r = parse_tree(right);
-        count += 1;
-        let correct = cmp(&l, &r);
-        if correct { sum += count; }
+        vec.push(parse_tree(left));
+        vec.push(parse_tree(right));
     }
-    sum
+    let marker1 = parse_tree("[[2]]");
+    let marker2 = parse_tree("[[6]]");
+    vec.push(marker1.clone());
+    vec.push(marker2.clone());
+
+    vec.sort_by(|a,b| cmp_ex(a,b));
+
+    let mut prod = 1;
+    for (c,x) in vec.iter().enumerate() {
+        if cmp_ex(x, &marker1) == Ordering::Equal
+            || cmp_ex(x, &marker2) == Ordering::Equal {
+                prod *= c+1;
+            }
+        // println!("{} {:?}", c, x);
+    }
+    prod
 }
 
 //------------------------------ RUNNERS
@@ -175,7 +185,7 @@ fn solve2(input: &'static str) -> usize {
 #[aoc(day13, part1)]
 fn day13_part1(input: &'static str) -> usize {
     let ans = solve1(input);
-    // assert_eq!(ans, 0);
+    assert_eq!(ans, 6395);
     ans
 }
 
@@ -183,14 +193,14 @@ fn day13_part1(input: &'static str) -> usize {
 #[aoc(day13, part2)]
 fn day13_part2(input: &'static str) -> usize {
     let ans = solve2(input);
-    // assert_eq!(ans, 0);
+    assert_eq!(ans, 117 * 213);
     ans
 }
 
 //------------------------------ TESTS
 
 #[test] fn test_day13_part1() { assert_eq!(solve1(_SAMPLE), 13); }
-#[test] fn test_day13_part2() { assert_eq!(solve2(_SAMPLE), _ANS2); }
+#[test] fn test_day13_part2() { assert_eq!(solve2(_SAMPLE), 140); }
 
 //------------------------------ SAMPLE DATA
 
