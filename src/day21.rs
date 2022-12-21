7646eaa8986588126a1d14610b35c84bc916c126
Phil Hord <phord@purestorage.com>
2022-12-20 23:49:15 -0800

    Day 21 part 2 - partially decomposed expression

--- src/day21.rs
+++ src/day21.rs
@@ -3,6 +3,7 @@ use yaah::aoc;
 #[allow(unused)]
 use crate::*;
 use std::collections::HashMap;
+use std::rc::Rc;
 
 //------------------------------ PARSE INPUT
 
@@ -12,10 +13,11 @@ enum Op {
     Minus,
     Times,
     Divide,
+    Equals,
 }
 #[derive(Clone, Debug)]
 enum Expr {
-    Some(Op, Box<Expr>, Box<Expr>),
+    Some(Op, Rc<Expr>, Rc<Expr>),
     Variable(&'static str),
     Literal(i64),
     Unknown(&'static str),
@@ -34,10 +36,10 @@ fn parse(input: &'static str) -> HashMap<&'static str, Expr> {
                 let a = Expr::Variable(l[1]);
                 let b = Expr::Variable(l[3]);
                 match l[2] {
-                    "+" => Expr::Some(Op::Plus,Box::new(a),Box::new(b)),
-                    "-" => Expr::Some(Op::Minus,Box::new(a),Box::new(b)),
-                    "*" => Expr::Some(Op::Times,Box::new(a),Box::new(b)),
-                    "/" => Expr::Some(Op::Divide,Box::new(a),Box::new(b)),
+                    "+" => Expr::Some(Op::Plus,Rc::new(a),Rc::new(b)),
+                    "-" => Expr::Some(Op::Minus,Rc::new(a),Rc::new(b)),
+                    "*" => Expr::Some(Op::Times,Rc::new(a),Rc::new(b)),
+                    "/" => Expr::Some(Op::Divide,Rc::new(a),Rc::new(b)),
                     _ => unimplemented!(),
                 }
             } else {
@@ -50,40 +52,52 @@ fn parse(input: &'static str) -> HashMap<&'static str, Expr> {
 
 //------------------------------ SOLVE
 #[test] fn test_day21_part1() { assert_eq!(solve1(_SAMPLE), 152); }
-#[test] fn test_day21_part2() { assert_eq!(solve2(_SAMPLE), _ANS2); }
 
 
-fn eval( eq: &Expr, vars: &HashMap<&'static str, Expr>) -> i64 {
+fn apply (a: &Expr, b: &Expr, f: fn(i64, i64) -> i64) -> Expr {
+    match (a, b) {
+        (Expr::Literal(x), Expr::Literal(y)) => Expr::Literal(f(*x,*y)),
+        (x,y) => Expr::Some(Op::Times, Rc::new(x.clone()), Rc::new(y.clone())),
+    }
+}
+
+fn try_eval( eq: &Expr, vars: &HashMap<&'static str, Expr>) -> Expr {
     let result = match eq {
-        Expr::Some(Op::Plus, a, b) => eval(&*a, vars) + eval(&*b, vars),
-        Expr::Some(Op::Minus, a, b) => eval(&*a, vars) - eval(&*b, vars),
-        Expr::Some(Op::Times, a, b) => eval(&*a, vars) * eval(&*b, vars),
-        Expr::Some(Op::Divide, a, b) => eval(&*a, vars) / eval(&*b, vars),
+        Expr::Some(Op::Plus, a, b) => apply(&try_eval(&*a, vars), &try_eval(&*b, vars), |x,y| x + y),
+        Expr::Some(Op::Minus, a, b) => apply(&try_eval(&*a, vars), &try_eval(&*b, vars), |x,y| x - y),
+        Expr::Some(Op::Times, a, b) => apply(&try_eval(&*a, vars), &try_eval(&*b, vars), |x,y| x * y),
+        Expr::Some(Op::Divide, a, b) => apply(&try_eval(&*a, vars), &try_eval(&*b, vars), |x,y| x / y),
+        Expr::Some(Op::Equals, _, _) => unimplemented!(),
 
-        // Expr::Some(Op::Plus, a, b) => Expr::Some(Op::Plus, eval(&*a, vars), eval(&*b, vars)),
-        // Expr::Some(Op::Minus, a, b) => Expr::Some(Op::Minus, eval(&*a, vars), eval(&*b, vars)),
-        // Expr::Some(Op::Times, a, b) => Expr::Some(Op::Times, eval(&*a, vars), eval(&*b, vars)),
-        // Expr::Some(Op::Divide, a, b) => Expr::Some(Op::Divide, eval(&*a, vars), eval(&*b, vars)),
-
-        Expr::Variable(x) => eval(&vars[x], &vars),
-        Expr::Literal(x) => *x,
-        Expr::Unknown(_) => panic!(),
+        Expr::Variable(x) => try_eval(&vars[x], &vars),
+        Expr::Literal(x) => Expr::Literal(*x),
+        Expr::Unknown(x) => Expr::Unknown(x),
     };
     result
 }
 
 
+fn eval( eq: &Expr, vars: &HashMap<&'static str, Expr>) -> i64 {
+    let e = try_eval(eq, vars);
+    match e {
+        Expr::Literal(x) => x,
+        _ => panic!(),
+    }
+}
 
-fn prt( eq: &Expr, vars: &HashMap<&'static str, Expr>) -> i64 {
+
+
+fn prt( eq: &Expr, vars: &HashMap<&'static str, Expr>) -> String {
     let result = match eq {
-        Expr::Some(Op::Plus, a, b) => eval(&*a, vars) + eval(&*b, vars),
-        Expr::Some(Op::Minus, a, b) => eval(&*a, vars) - eval(&*b, vars),
-        Expr::Some(Op::Times, a, b) => eval(&*a, vars) * eval(&*b, vars),
-        Expr::Some(Op::Divide, a, b) => eval(&*a, vars) / eval(&*b, vars),
+        Expr::Some(Op::Plus, a, b) => "( ".to_string() + &prt(&*a, vars) + " + " + &prt(&*b, vars) + " )",
+        Expr::Some(Op::Minus, a, b) => "( ".to_string() + &prt(&*a, vars) + " - " + &prt(&*b, vars) + " )",
+        Expr::Some(Op::Times, a, b) => "( ".to_string() + &prt(&*a, vars) + " * " + &prt(&*b, vars) + " )",
+        Expr::Some(Op::Divide, a, b) => "( ".to_string() + &prt(&*a, vars) + " / " +  &prt(&*b, vars) + " )",
+        Expr::Some(Op::Equals, a, b) => "( ".to_string() + &prt(&*a, vars) + " = " +  &prt(&*b, vars) + " )",
 
-        Expr::Variable(x) => eval(&vars[x], &vars),
-        Expr::Literal(x) => *x,
-        Expr::Unknown(_) => panic!(),
+        Expr::Variable(x) => prt(&vars[*x], vars),
+        Expr::Literal(x) => format!(" {} ", x),
+        Expr::Unknown(x) => x.to_string(),
     };
     result
 }
@@ -93,10 +107,20 @@ fn solve1(input: &'static str) -> i64 {
 
     eval(&Expr::Variable("root"), &sim)
 }
+#[test] fn test_day21_part2() { assert_eq!(solve2(_SAMPLE), _ANS2); }
 
 fn solve2(input: &'static str) -> i64 {
     let mut sim = parse(input);
-    eval(&Expr::Variable("root"), &sim)
+
+    sim.insert("humn", Expr::Unknown("humn"));
+
+    match &sim["root"] {
+        Expr::Some(_, a, b) =>  println!("{} == {}", prt(&try_eval(&*a, &sim), &sim), prt(&try_eval(&*b, &sim), &sim) ),
+        _ => panic!(),
+    };
+
+    // println!("{}", prt(&Expr::Variable("root"), &sim));
+    0
 }
 
 //------------------------------ RUNNERS
@@ -110,7 +134,7 @@ fn day21_part1(input: &'static str) -> i64 {
 }
 
 #[allow(unused)]
-// #[aoc(day21, part2)]
+#[aoc(day21, part2)]
 fn day21_part2(input: &'static str) -> i64 {
     let ans = solve2(input);
     // assert_eq!(ans, 0);
